cc54eb66546d290158451807db11a2c59dbfd7c1
Abby <me@abbyjanke.com>
2020-05-16 21:46:07 -0700

    Prettier Commands, Ask ?s instead of assuming.

--- src/app/Console/Commands/InstallCommand.php
+++ src/app/Console/Commands/InstallCommand.php
@@ -2,8 +2,8 @@
 
 namespace AbbyJanke\Expensed\App\Console\Commands;
 
-use Illuminate\Console\Command;
 use AbbyJanke\Expensed\App\Models\Currency;
+use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 
 class InstallCommand extends Command
@@ -13,7 +13,7 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'backpack:install-expensed {categories=true}';
+    protected $name = 'backpack:install-expensed';
 
     /**
      * The console command description.
@@ -39,16 +39,44 @@ class InstallCommand extends Command
      */
     public function handle()
     {
-        Artisan::call('database:migrate'); // Add new database tables
+        $bar = $this->output->createProgressBar(4);
+
+        // using permission manager?
+        $usePermissionManager = $this->confirm('Use Backpack/PermissionManager to restrict access?', 'yes');
+        if($usePermissionManager) {
+            $this->info("\n".'Installing Backpack/PermissionManager');
+            $command = 'composer require backpack/permissionmanager --prefer-source';
+            shell_exec($command);
+        } else {
+            $this->comment('Skipping Backpack/PermissionManager');
+        }
+        $bar->advance();
+
+        // Add new Database tables
+        Artisan::call('migrate');
+        $this->info("\n".'New Tables Migrated.');
+        $bar->advance();
 
         // Check to see if the API Token is set
-        if(!is_null(config('backpack.expensed.api_token'))) {
-            Artisan::call('currency:install'); // Add the currency
+        $currencies = Currency::all()->count();
+        if($currencies == 0 && $this->confirm('Install/Update currency rates using OpenExchange.com?', 'yes')) {
+            $this->info("\n".'Installing/Updating Currencies');
+            Artisan::call('currency:update'); // Add the currency
+        } else {
+            $this->error('Currencies not updated, you will need to manually update them or add an API Token and run "php artisan currency:install"');
         }
+        $bar->advance();
+
         // double check to make sure they want default categories
-        if($this->argument('categories')) {
+        if($this->confirm('Install Default Categories?')) {
+            $this->info("\n".'Adding Default Categories');
             Artisan::call('db:seed', ['class' => 'AbbyJanke\Expensed\Database\Seeds\CategoriesTableSeeder']);
+        } else {
+            $this->info("\n".'Skipping Default Categories');
         }
+
+        $bar->finish();
+        return;
     }
 
 }
